9b0959442194d84fd24cb10af7079fe4ef98f38a
Jarom Plewe <jplewe23@gmail.com>
2020-10-30 00:12:06 -0600

    added functionality to append choices depending on user input

--- public/assets/js/frugal.js
+++ public/assets/js/frugal.js
@@ -1,22 +1,38 @@
-// click listeners
-// drop down selections
-
-// calculator
-
-// moment.js
-console.log("Hello");
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').formSelect();
-    
-    $("#incomeType").on("change", function(){
-      let value = parseInt($(this).val())
-      console.log(value)
-      if (value === 1) {
-        $("#incomeFrequency").removeClass("hidden");
-      }
+
+    $("#employmentStatus").on("change", function () {
+
+        let value = parseInt($(this).val());
+
+        if (value === 1) {
+            $("#paidHourly").removeClass("hidden");
+        };
+    });
+
+    $("#paidHourlyChoice").on("change", function () {
+        let value = parseInt($(this).val());
+
+        if (value === 1) {
+            $("#payAmmountContainer").empty();
+            $("#payAmmountContainer").append(hourlyIncomeForm)
+        } else if (value === 2) {
+            $("#payAmmountContainer").empty();
+            $("#payAmmountContainer").append(annualIncomeForm)
+        };
     });
 
 
-    
-  });
+});
+
+let annualIncomeForm = `
+<div id="annualIncome" class="input-field col s6">
+    <input placeholder="" type="text" class="validate">
+    <label for="first_name">What is your annual salary?</label>
+</div>`;
+
+let hourlyIncomeForm = `
+<div id="hourlyIncome" class="input-field col s6">
+    <input placeholder="" type="text" class="validate">
+    <label for="first_name">What is your hourly rate?</label>
+</div>`;
