44a4deec3f778d1ed7aa086896c5647a5afb142e
Jarom Plewe <jplewe23@gmail.com>
2020-11-05 17:26:42 -0700

    added doughnut chart with dummy data

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -18,15 +18,14 @@ let appendIncome = async () => {
     let recIncObj = await recurringIncome()
     for (let i = 0; i < recIncObj.length; i++) {
         recIncNameDisplay = recIncObj[i].recurring_income_name;
-        recIncAmtDisplay = recIncObj[i].recurring_income_ammt;
+        recIncAmtDisplay = recIncObj[i].recurring_income_ammt / 12;
 
-        let incomeCard = `<a class="collection-item"><span class="new badge" data-badge-caption="">$ ${ recIncAmtDisplay }</span>${ recIncNameDisplay }</a>`
+        let incomeCard = `<a class="collection-item"><span class="new badge" data-badge-caption="">$ ${ recIncAmtDisplay.toFixed(2) }</span>${ recIncNameDisplay }</a>`
         $("#recIncContainer").append(incomeCard)
     }
 
     // append static income
     let statIncObj = await staticIncome()
-    console.log(statIncObj)
     for (let i = 0; i < statIncObj.length; i++) {
         statIncNameDisplay = statIncObj[i].static_income_name;
         statIncAmtDisplay = statIncObj[i].static_income_ammt;
@@ -42,27 +41,60 @@ let appendExpense = async () => {
     
     // append recurring expense
     let recExpObj = await recurringExpense()
-    console.log(recExpObj)
     for (let i = 0; i < recExpObj.length; i++) {
         recExpNameDisplay = recExpObj[i].recurring_expense_name;
-        recExpAmtDisplay = recExpObj[i].recurring_expense_ammt;
+        recExpAmtDisplay = recExpObj[i].recurring_expense_ammt / 12;
 
-        let ExpenseCard = `<a class="collection-item"><span class="new badge red darken-1" data-badge-caption="">$ ${ recExpAmtDisplay }</span>${ recExpNameDisplay }</a>`
+        let ExpenseCard = `<a class="collection-item"><span class="new badge red darken-1">$ ${ recExpAmtDisplay.toFixed(2) }</span>${ recExpNameDisplay }</a>`
         $("#recExpContainer").append(ExpenseCard)
     }
 
     // append static Expense
     let statExpObj = await staticExpense()
-    console.log(statExpObj)
     for (let i = 0; i < statExpObj.length; i++) {
         statExpNameDisplay = statExpObj[i].static_expense_name;
         statExpAmtDisplay = statExpObj[i].static_expense_ammt;
 
-        let ExpenseCard = `<a class="collection-item"><span class="new badge red darken-1" data-badge-caption="">$ ${ statExpAmtDisplay }</span>${ statExpNameDisplay }</a>`
+        let ExpenseCard = `<a class="collection-item"><span class="new badge red darken-1">$ ${ statExpAmtDisplay }</span>${ statExpNameDisplay }</a>`
         $("#statExpContainer").append(ExpenseCard)
     }
 };
 appendExpense()
+
+// pie chart calculations
+let pieChartCalculations = async () => {
+    console.log('ben')
+    let recIncObj = await recurringIncome()
+    let statIncObj = await staticIncome()
+    let recIncAmt;
+    for (let i=0; i<recIncObj.length; i++) {
+        console.log(parseInt(recIncObj[i].recurring_income_ammt))
+        recIncAmt += parseInt(recIncObj[i].recurring_income_ammt);
+        console.log("Total recurring income amount: " + recIncAmt)
+    }
+    console.log(recIncAmt)
+}
+pieChartCalculations()
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Bob's Burgers", "awef", "awef", "Purple", "Yellow", "Red", "Black"],
+    datasets: [{
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db",
+        "#95a5a6",
+        "#9b59b6",
+        "#f1c40f",
+        "#e74c3c",
+        "#34495e"
+      ],
+      data: [40, 19, 3, 17, 28, 24, 7]
+    }]
+  }
+});
 
 
 
